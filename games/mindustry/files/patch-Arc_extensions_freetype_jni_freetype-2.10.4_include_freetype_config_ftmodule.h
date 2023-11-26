286619dcb466bfb7c3e96bfd04a803b5707eeeb8
Er2 <er2@dismail.de>
2023-11-26 17:06:01 +0300

    games/mindustry: add port

---
+++ games/mindustry/files/patch-Arc_extensions_freetype_jni_freetype-2.10.4_include_freetype_config_ftmodule.h
@@ -0,0 +1,22 @@
+--- ../Arc/extensions/freetype/jni/freetype-2.10.4/include/freetype/config/ftmodule.h.orig	2023-11-26 13:46 UTC
++++ ../Arc/extensions/freetype/jni/freetype-2.10.4/include/freetype/config/ftmodule.h
+@@ -12,19 +12,8 @@ FT_USE_MODULE( FT_Driver_ClassRec, tt_driver_class )
+ 
+ FT_USE_MODULE( FT_Module_Class, autofit_module_class )
+ FT_USE_MODULE( FT_Driver_ClassRec, tt_driver_class )
+-FT_USE_MODULE( FT_Driver_ClassRec, t1_driver_class )
+-FT_USE_MODULE( FT_Driver_ClassRec, cff_driver_class )
+-FT_USE_MODULE( FT_Driver_ClassRec, t1cid_driver_class )
+-FT_USE_MODULE( FT_Driver_ClassRec, pfr_driver_class )
+-FT_USE_MODULE( FT_Driver_ClassRec, t42_driver_class )
+-FT_USE_MODULE( FT_Driver_ClassRec, winfnt_driver_class )
+-FT_USE_MODULE( FT_Driver_ClassRec, pcf_driver_class )
+-FT_USE_MODULE( FT_Module_Class, psaux_module_class )
+ FT_USE_MODULE( FT_Module_Class, psnames_module_class )
+-FT_USE_MODULE( FT_Module_Class, pshinter_module_class )
+-FT_USE_MODULE( FT_Renderer_Class, ft_raster1_renderer_class )
+ FT_USE_MODULE( FT_Module_Class, sfnt_module_class )
+ FT_USE_MODULE( FT_Renderer_Class, ft_smooth_renderer_class )
+-FT_USE_MODULE( FT_Driver_ClassRec, bdf_driver_class )
+ 
+ /* EOF */
