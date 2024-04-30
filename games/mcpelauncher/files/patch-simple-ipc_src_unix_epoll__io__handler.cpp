512ed62013b1c98e29347202123ce7e245497417
Er2 <er2@dismail.de>
2024-04-30 13:20:34 +0300

    games/mcpelauncher: Upgrade to 0.14.3

--- games/mcpelauncher/files/patch-simple-ipc_src_unix_epoll__io__handler.cpp
+++
@@ -1,9 +0,0 @@
---- simple-ipc/src/unix/epoll_io_handler.cpp.orig	2023-03-25 17:39:55 UTC
-+++ simple-ipc/src/unix/epoll_io_handler.cpp
-@@ -90,4 +90,4 @@ io_handler& io_handler::get_instance() {
- io_handler& io_handler::get_instance() {
-     static epoll_io_handler instance;
-     return instance;
--}
-\ No newline at end of file
-+}
