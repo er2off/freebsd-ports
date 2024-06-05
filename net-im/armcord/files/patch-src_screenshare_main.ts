ccf47fc64862d66d35f8cbfe78532d96223af01f
Er2 <er2@dismail.de>
2024-06-05 14:06:04 +0300

    net-im/armcord: upgrade to 3.2.7

--- net-im/armcord/files/patch-src_screenshare_main.ts
+++ net-im/armcord/files/patch-src_screenshare_main.ts
@@ -1,11 +1,11 @@
---- src/screenshare/main.ts.orig	2024-02-12 15:01:34 UTC
+--- src/screenshare/main.ts.orig	2024-05-18 11:00:04 UTC
 +++ src/screenshare/main.ts
-@@ -17,7 +17,7 @@ function registerCustomHandler(): void {
+@@ -29,7 +29,7 @@ function registerCustomHandler(): void {
              types: ["screen", "window"]
          });
          console.log(sources);
 -        if (process.platform === "linux" && process.env.XDG_SESSION_TYPE?.toLowerCase() === "wayland") {
 +        if (process.platform in ["linux", "freebsd"] && process.env.XDG_SESSION_TYPE?.toLowerCase() === "wayland") {
              console.log("WebRTC Capturer detected, skipping window creation."); //assume webrtc capturer is used
-             console.log({video: {id: sources[0].id, name: sources[0].name}});
-             callback({video: {id: sources[0].id, name: sources[0].name}});
+             var options: Electron.Streams = {video: sources[0]};
+             if (showAudioDialog() == true) options = {video: sources[0], audio: "loopbackWithMute"};
